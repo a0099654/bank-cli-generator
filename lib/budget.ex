080ae1ff3b7f9410a46cfed13c61d1991513586e
Stella Widyasari <stella.widyasari@gmail.com>
2018-01-08 11:03:44 +0800

    [Stella]Read the csv file and print

--- lib/budget.ex
+++ lib/budget.ex
@@ -1,5 +1,5 @@
 defmodule Budget do
   def listing_transactions do
-    "listing transactions"
+    File.read("lib/transactions.csv")
   end
 end
--- lib/mix/tasks/listing_transactions.ex
+++ lib/mix/tasks/listing_transactions.ex
@@ -1,5 +1,6 @@
 defmodule Mix.Tasks.ListingTransactions do
     use Mix.Task
+    @shortdoc "List transactions from a CSV file"
     def run(_) do
         Budget.listing_transactions |> IO.inspect
     end
